50d4293c360893e0a7279d8687d64564957bf883
Hjemmeseier <50299192+Hjemmeseier@users.noreply.github.com>
2024-11-09 08:54:29 +0100

    Update pdf_utils.py

--- pdf_utils.py
+++ pdf_utils.py
@@ -1,17 +1,31 @@
-from PyPDF2 import PdfMerger
+from PyPDF2 import PdfReader, PdfWriter, PdfMerger
 import io
 
-def merge_pdfs(files):
-    # Create a PDF merger object
-    merger = PdfMerger()
+def extract_pages(pdf_file, start_page, end_page):
+    reader = PdfReader(pdf_file)
+    writer = PdfWriter()
+    
+    num_pages = len(reader.pages)
+    start_page = max(1, start_page)
+    end_page = min(end_page, num_pages)
+    
+    for page_num in range(start_page - 1, end_page):
+        writer.add_page(reader.pages[page_num])
+    
+    extracted_pdf = io.BytesIO()
+    writer.write(extracted_pdf)
+    extracted_pdf.seek(0)
+    
+    return extracted_pdf
 
-    # Loop through selected files and merge them
+def merge_pdfs(files):
+    merger = PdfMerger()
     for file in files:
         merger.append(file)
-
-    # Save the merged PDF to a BytesIO object
+    
     merged_pdf = io.BytesIO()
     merger.write(merged_pdf)
     merger.close()
-
+    merged_pdf.seek(0)
+    
     return merged_pdf
